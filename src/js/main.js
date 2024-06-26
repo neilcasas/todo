1bc450726952c08c2f1039ed94acd48930454ca6
Neil Casas <neilalfonzcasas@gmail.com>
2024-06-26 22:03:33 +0800

    created todo component

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,6 @@
 import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
+import loadPage from './uicontroller.js'
+
+
+loadPage();
--- src/js/uicontroller.js
+++ src/js/uicontroller.js
@@ -1,9 +1,49 @@
 // This module contains the logic for displaying and manipulating DOM elements
+import { ToDoList, Project } from "./todolist";
+import { Todo } from "./todo";
+
+
+const bodyElement = document.querySelector('body');
+const contentElement = document.querySelector('#main-container')
+
+
+// Navbar component
+const navbar = () => {
+    const nav = document.createElement('nav');
+    nav.classList.add('navbar');
+    const text = document.createElement('h1');
+    text.textContent = 'Today';
+    nav.appendChild(text);
+    return nav;
+}
+
+// Todo component
+const todoComponent = (todo) => {
+    const todoElement = document.createElement('div');
+    todoElement.classList.add('card');
+
+    const todoBody = document.createElement('div');
+    todoBody.classList.add('card-body');
+
+    const todoTitle = document.createElement('div');
+    todoTitle.classList.add('card-title');
+    todoTitle.textContent = todo.getTitle();
+    todoBody.appendChild(todoTitle);
+
+    const todoDescription = todo.getDescription();
+    todoBody.textContent = todoDescription;
+
+    return todoElement;
+}
 
 // View all todos within a project
-
 
 
 // View all projects
 // Expand a todo
 // Delete a todo
+
+
+export default function loadPage() {
+    bodyElement.appendChild(navbar())
+}
